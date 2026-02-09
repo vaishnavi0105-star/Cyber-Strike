5276225ef31241c619ea7d368554d081a7ab57ce
vaishnavi0105-star <vaishnavithakur0105@gmail.com>
2026-02-09 18:02:42 +0530

    Fix Vercel deployment: serve static files from Express server

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const routes = require("./routes");
 
@@ -7,6 +8,15 @@ const app = express();
 app.use(cors({ origin: true }));
 app.use(express.json());
 
+// API routes
 app.use("/api", routes);
+
+// Serve static files from root directory
+app.use(express.static(path.join(__dirname, "..")));
+
+// Handle SPA routing - serve index.html for all non-API routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "index.html"));
+});
 
 module.exports = app;
